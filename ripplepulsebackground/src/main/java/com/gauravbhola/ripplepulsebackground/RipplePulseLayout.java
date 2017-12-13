5f30d13d8c050cc3c0ff7beb9d6e4c9cc36a4f52
Gaurav Bhola <gaurav414u@gmail.com>
2017-12-13 21:59:38 +0530

    Primary functionality complete

--- ripplepulsebackground/src/main/java/com/gauravbhola/ripplepulsebackground/RipplePulseLayout.java
+++ ripplepulsebackground/src/main/java/com/gauravbhola/ripplepulsebackground/RipplePulseLayout.java
@@ -1,16 +1,23 @@
 package com.gauravbhola.ripplepulsebackground;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+import android.support.v7.widget.FitWindowsLinearLayout;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 
 /**
@@ -18,8 +25,15 @@ import android.widget.RelativeLayout;
  */
 
 public class RipplePulseLayout extends RelativeLayout {
+    public static final int DEFAULT_DURATION = 2000;
+    public static final String RIPPLE_TYPE_FILL = "0";
+    public static final String RIPPLE_TYPE_STROKE = "1";
     public static final String TAG = "RipplePulseLayout";
+
     private Paint mPaint;
+    private AnimatorSet mAnimatorSet;
+    private boolean mIsAnimating;
+
 
     public RipplePulseLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -46,44 +60,89 @@ public class RipplePulseLayout extends RelativeLayout {
         if (isInEditMode()) {
             return;
         }
+        //reading the attributes
         TypedArray attrValues = context.obtainStyledAttributes(attrs, R.styleable.RipplePulseLayout);
-        int color = attrValues.getColor(R.styleable.RipplePulseLayout_rippleColor, getResources().getColor(android.R.color.black));
-        initializePaint(color);
-        Log.d(TAG, "init");
+        int color = attrValues.getColor(R.styleable.RipplePulseLayout_rippleColor, getResources().getColor(android.R.color.holo_blue_bright));
+        float startRadius = attrValues.getDimension(R.styleable.RipplePulseLayout_startRadius, getMeasuredWidth()/2);
+        float endRadius = attrValues.getDimension(R.styleable.RipplePulseLayout_endRadius, getMeasuredWidth());
+        float strokeWidth = attrValues.getDimension(R.styleable.RipplePulseLayout_strokeWidth, 4);
+        int duration = attrValues.getInteger(R.styleable.RipplePulseLayout_duration, DEFAULT_DURATION);
+        String rippleType = attrValues.getString(R.styleable.RipplePulseLayout_rippleType);
+        if (TextUtils.isEmpty(rippleType)) {
+            rippleType = RIPPLE_TYPE_FILL;
+        }
 
-        RippleView rp = new RippleView(getContext(), mPaint);
-        LayoutParams params = new LayoutParams(300, 300);
+        initializePaint(color, rippleType, strokeWidth);
+
+        RippleView rp = new RippleView(getContext(), mPaint, startRadius);
+        LayoutParams params = new LayoutParams((int)endRadius, (int)endRadius);
         params.addRule(CENTER_IN_PARENT, TRUE);
         addView(rp, params);
+
+        mAnimatorSet = new AnimatorSet();
+        ObjectAnimator scaleXAnimator = ObjectAnimator.ofFloat(rp, "scaleX", 1f, endRadius/startRadius);
+        scaleXAnimator.setRepeatCount(ValueAnimator.INFINITE);
+
+        ObjectAnimator scaleYAnimator = ObjectAnimator.ofFloat(rp, "scaleY", 1f, endRadius/startRadius);
+        scaleYAnimator.setRepeatCount(ValueAnimator.INFINITE);
+
+        ObjectAnimator alphaAnimator = ObjectAnimator.ofFloat(rp, "alpha", 1f, 0f);
+        alphaAnimator.setRepeatCount(ValueAnimator.INFINITE);
+
+        mAnimatorSet.setDuration(duration);
+        mAnimatorSet.setInterpolator(new AccelerateDecelerateInterpolator());
+        mAnimatorSet.playTogether(scaleXAnimator, scaleYAnimator, alphaAnimator);
+    }
+
+    public void startRippleAnimation() {
+        if (mIsAnimating) {
+            //already animating
+            return;
+        }
+        mAnimatorSet.start();
+        mIsAnimating = true;
+    }
+
+    public void stopRippleAnimation() {
+        if (!mIsAnimating) {
+            //already not animating
+            return;
+        }
+        mAnimatorSet.end();
+        mIsAnimating = false;
     }
 
     /**
      * Initializes the paint with the required properties
      * @param color
      */
-    private void initializePaint(int color) {
+    private void initializePaint(int color, String rippleType, float strokeWidth) {
         mPaint = new Paint();
         mPaint.setColor(color);
         mPaint.setAntiAlias(true);
-        mPaint.setStyle(Paint.Style.STROKE);
-        mPaint.setStrokeWidth(3);
+        if (rippleType.equals(RIPPLE_TYPE_STROKE)) {
+            mPaint.setStyle(Paint.Style.STROKE);
+            mPaint.setStrokeWidth(strokeWidth);
+        } else {
+            mPaint.setStyle(Paint.Style.FILL);
+            mPaint.setStrokeWidth(0);
+        }
     }
 
     private static class RippleView extends View {
         private Paint mPaint;
+        private float mRadius;
 
-        public RippleView(Context context, Paint p) {
+        public RippleView(Context context, Paint p, float radius) {
             super(context);
             mPaint = p;
+            mRadius = radius;
         }
 
         @Override
         protected void onDraw(Canvas canvas) {
-            int radius=(Math.min(getWidth(),getHeight()))/2;
-            canvas.drawCircle(radius,
-                    radius,
-                    200,
-                    mPaint);
+            int center=(Math.min(getWidth(),getHeight()))/2;
+            canvas.drawCircle(center, center, mRadius, mPaint);
         }
     }
 }
--- sample/src/main/java/com/gauravbhola/ripplepulsebackground/sample/MainActivity.java
+++ sample/src/main/java/com/gauravbhola/ripplepulsebackground/sample/MainActivity.java
@@ -1,14 +1,20 @@
 package com.gauravbhola.ripplepulsebackground.sample;
 
+import com.gauravbhola.ripplepulsebackground.RipplePulseLayout;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    RipplePulseLayout mRipplePulseLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRipplePulseLayout = findViewById(R.id.layout_ripplepulse);
+        mRipplePulseLayout.startRippleAnimation();
+        mRipplePulseLayout.stopRippleAnimation();
     }
 
 }
