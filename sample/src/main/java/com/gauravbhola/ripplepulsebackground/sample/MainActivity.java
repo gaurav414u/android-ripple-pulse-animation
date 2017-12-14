b6ad92494a6dc7c3e30173708d62ae5b98458b19
Gaurav Bhola <gaurav414u@gmail.com>
2017-12-14 23:07:40 +0530

    Making sample more beautiful which shows both ripple types fill and stroke

--- sample/src/main/java/com/gauravbhola/ripplepulsebackground/sample/MainActivity.java
+++ sample/src/main/java/com/gauravbhola/ripplepulsebackground/sample/MainActivity.java
@@ -7,13 +7,16 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     RipplePulseLayout mRipplePulseLayout;
+    RipplePulseLayout mRipplePulseLayout2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mRipplePulseLayout = findViewById(R.id.layout_ripplepulse);
+        mRipplePulseLayout2 = findViewById(R.id.layout_ripplepulse_2);
         mRipplePulseLayout.startRippleAnimation();
+        mRipplePulseLayout2.startRippleAnimation();
 //        mRipplePulseLayout.stopRippleAnimation();
     }
 
